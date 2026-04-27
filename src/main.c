e02aa29a3366bd38ac5ea061fbeb1ba55995413c
duyang <kamutulafu@outlook.com>
2026-04-27 19:19:15 +0800

    添加打印机DEMO测试程序

--- src/main.c
+++ src/main.c
@@ -25,6 +25,7 @@
 #include <SDL.h>
 
 #include "hal/hal.h"
+#include "ui.h"
 
 /*********************
  *      DEFINES
@@ -61,15 +62,10 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  sdl_hal_init(320, 480);
+  sdl_hal_init(1024, 600);
 
-  /* Run the default demo */
-  /* To try a different demo or example, replace this with one of: */
-  /* - lv_demo_benchmark(); */
-  /* - lv_demo_stress(); */
-  /* - lv_example_label_1(); */
-  /* - etc. */
-  lv_demo_widgets();
+  /* Run the SquareLine UI */
+  ui_init();
 
   while(1) {
     /* Periodically call the lv_task handler.
